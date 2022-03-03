7bbd3c1635f21729a6af835e0b6d8cac2731744e
tofatoff <mail.mstofa@gmail.com>
2022-03-03 11:13:08 +0700

    first version of redirector

---
+++ app/src/main/java/com/example/whatsappredirector/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.whatsappredirector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+    }
+
+    public void openWebsite(View view) {
+        String url = "https://wa.me/"+mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+}
